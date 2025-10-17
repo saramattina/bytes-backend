4ea63961b830c66fd0273900d696fcbb5926aadd
danny amit <danny@iamit.org>
2025-10-17 15:47:43 -0400

    added models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    notes = models.TextField(max_length=250)
+    favorite = models.BooleanField(default=False)
+    image = models.ImageField(upload_to="recipes/", blank=True, null=True)
+
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    quantity = models.FloatField()
+
+    class VolumeUnits(models.TextChoices):
+        CUP = "cup", "Cup"
+        TBSP = "tbsp", "Tablespoon"
+        TSP = "tsp", "Teaspoon"
+
+    class WeightUnits(models.TextChoices):
+        G = "g", "Gram"
+        OZ = "oz", "Ounce"
+
+    volume_unit = models.CharField(
+        max_length=10, choices=VolumeUnits.choices, blank=True, null=True
+    )
+    weight_unit = models.CharField(
+        max_length=10, choices=WeightUnits.choices, blank=True, null=True
+    )
+
+
+class Steps(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    step = models.IntegerField()
+    description = models.TextField(max_length=250)
