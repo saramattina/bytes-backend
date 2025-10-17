5f2f287a5fb8d477ad2c9416b359203ffe1d2d22
Sara Mattina <smattina@gmail.com>
2025-10-17 16:50:43 -0400

    begin urls and views

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import Home, LoginView, SignUp, VerifyUserView, RecipeList, RecipeDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", Home.as_view(), name="home"),
+    path("users/login/", LoginView.as_view(), name="login"),
+    path("users/sign-up/", SignUp.as_view(), name="sign-up"),
+    path('users/token/refresh/', VerifyUserView.as_view(), name='token_refresh'),
+    path("recipes/", RecipeList.as_view(), name="recipe-list"),
+    path("reciples/<int:id>", RecipeDetail.as_view(), name="recipe-detail"),
 ]
