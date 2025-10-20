c38fdee56a738fe13d6c879911efc8768317fe1f
Dylan Tai <dylztai@gmail.com>
2025-10-20 09:28:46 -0400

    REST_FRAMEWORK pushed

--- recipecollector/settings.py
+++ recipecollector/settings.py
@@ -43,6 +43,12 @@ INSTALLED_APPS = [
     'main_app',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
