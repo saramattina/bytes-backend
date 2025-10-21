5363a41aa09903a7c30174a171d6f548736debc1
Dylan Tai <dylztai@gmail.com>
2025-10-21 09:35:56 -0400

    Made password functionality easier for making new user

--- recipecollector/settings.py
+++ recipecollector/settings.py
@@ -99,6 +99,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
+"""
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
+"""
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
