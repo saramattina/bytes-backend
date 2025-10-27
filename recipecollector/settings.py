b9d2f25763a46e5995606ed7b386e865ce0ea10d
Sara Mattina <smattina@gmail.com>
2025-10-27 17:23:05 -0400

    update settings

--- recipecollector/settings.py
+++ recipecollector/settings.py
@@ -38,7 +38,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['bytes-backend-production.up.railway.app', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://bytes-backend-production.up.railway.app",
+    "bytes-backend-production.up.railway.app",
 ]
 
 
